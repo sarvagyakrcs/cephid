b9ae6d36f7632b95da872e4a76747b254ea094e4
Sarvagya Kumar <sarvagyakrcs@gmail.com>
2025-12-11 16:42:03 +0530

    chore: update Kotlin plugin versions to 2.0.21 in build.gradle.kts

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "1.9.25"
-    kotlin("plugin.spring") version "1.9.25"
+    kotlin("jvm") version "2.0.21"
+    kotlin("plugin.spring") version "2.0.21"
     id("org.springframework.boot") version "3.4.0"
     id("io.spring.dependency-management") version "1.1.5"
     id("com.google.protobuf") version "0.9.4"
