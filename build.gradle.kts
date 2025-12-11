ac23bfbc6324b19015015518fc767bf25236c77c
Sarvagya Kumar <sarvagyakrcs@gmail.com>
2025-12-11 14:02:58 +0530

    feat: implement gRPC PingService and update build configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,6 @@ plugins {
     kotlin("plugin.spring") version "1.9.25"
     id("org.springframework.boot") version "3.4.0"
     id("io.spring.dependency-management") version "1.1.5"
-    //protobuf plugin
     id("com.google.protobuf") version "0.9.4"
 }
 
@@ -25,7 +24,7 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
 
-    // gRPC & Protobuf Dependencies
+    // gRPC & Protobuf
     implementation("net.devh:grpc-server-spring-boot-starter:3.1.0.RELEASE")
     implementation("io.grpc:grpc-kotlin-stub:1.4.1")
     implementation("com.google.protobuf:protobuf-kotlin:3.25.1")
@@ -33,13 +32,18 @@ dependencies {
     // JSR-330
     implementation("javax.inject:javax.inject:1")
     implementation("jakarta.inject:jakarta.inject-api:2.0.1")
+
+    // Testing
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 protobuf {
     protoc {
         artifact = "com.google.protobuf:protoc:3.25.1"
     }
+
     plugins {
         create("grpc") {
             artifact = "io.grpc:protoc-gen-grpc-java:1.60.0"
@@ -48,15 +52,33 @@ protobuf {
             artifact = "io.grpc:protoc-gen-grpc-kotlin:1.4.1:jdk8@jar"
         }
     }
+
     generateProtoTasks {
-        all().forEach {
-            it.plugins {
+        all().forEach { task ->
+            task.plugins {
                 create("grpc")
                 create("grpckt")
             }
-            it.builtins {
+            task.builtins {
                 create("kotlin")
             }
         }
     }
+}
+
+
+sourceSets {
+    main {
+        java.srcDirs(
+            "build/generated/source/proto/main/grpc",
+            "build/generated/source/proto/main/grpckt",
+            "build/generated/source/proto/main/java",
+            "build/generated/source/proto/main/kotlin"
+        )
+    }
+}
+
+tasks.test {
+    useJUnitPlatform()
+    failOnNoDiscoveredTests = false
 }
