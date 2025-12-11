740f7954a2fe06dfd6f09dba29421b3f90fb050f
Sarvagya Kumar <sarvagyakrcs@gmail.com>
2025-12-11 13:27:38 +0530

    feat: update dependencies and add gRPC support in build configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,14 @@
 plugins {
-    kotlin("jvm") version "2.2.21"
-    kotlin("plugin.spring") version "2.2.21"
-    id("org.springframework.boot") version "4.0.0"
-    id("io.spring.dependency-management") version "1.1.7"
+    kotlin("jvm") version "1.9.25"
+    kotlin("plugin.spring") version "1.9.25"
+    id("org.springframework.boot") version "3.4.0"
+    id("io.spring.dependency-management") version "1.1.5"
+    //protobuf plugin
+    id("com.google.protobuf") version "0.9.4"
 }
 
-group = "org.example"
+group = "com.example"
 version = "0.0.1-SNAPSHOT"
-description = "cephid"
 
 java {
     toolchain {
@@ -20,20 +21,42 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-webmvc")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("tools.jackson.module:jackson-module-kotlin")
-    testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    // gRPC & Protobuf Dependencies
+    implementation("net.devh:grpc-server-spring-boot-starter:3.1.0.RELEASE")
+    implementation("io.grpc:grpc-kotlin-stub:1.4.1")
+    implementation("com.google.protobuf:protobuf-kotlin:3.25.1")
+
+    // JSR-330
+    implementation("javax.inject:javax.inject:1")
+    implementation("jakarta.inject:jakarta.inject-api:2.0.1")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
-kotlin {
-    compilerOptions {
-        freeCompilerArgs.addAll("-Xjsr305=strict", "-Xannotation-default-target=param-property")
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:3.25.1"
     }
-}
-
-tasks.withType<Test> {
-    useJUnitPlatform()
+    plugins {
+        create("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:1.60.0"
+        }
+        create("grpckt") {
+            artifact = "io.grpc:protoc-gen-grpc-kotlin:1.4.1:jdk8@jar"
+        }
+    }
+    generateProtoTasks {
+        all().forEach {
+            it.plugins {
+                create("grpc")
+                create("grpckt")
+            }
+            it.builtins {
+                create("kotlin")
+            }
+        }
+    }
 }
